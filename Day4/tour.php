0fcd580564adb588637d1ab89b608b5457b814af
krupa2599 <84760575+krupa2599@users.noreply.github.com>
2021-06-03 12:39:54 +0530

    Add files via upload

---
+++ Day4/tour.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+ <title>Webpage design</title> 
+ <style type="text/css"> 
+ *{ 
+ margin: 0px; 
+ padding:0px; 
+ } 
+ body{ 
+ background-color: white; 
+ } 
+ header{ 
+ width: 100%; 
+ height: 50px; 
+ } 
+ .first 
+ { 
+ margin-top: 5px; 
+ width: 100%; 
+ height: 50px; 
+}
+.logo{ 
+ 
+ font-size: 20px; 
+ float: left; 
+ text-shadow: 2px 3px 1px 0px green; 
+ margin-left: 20px; 
+ margin-top: 5px; 
+ } 
+ .social{ 
+ float: right; 
+ height: 25px; 
+ margin-right: 20px; 
+ margin-top: 10px; 
+ font-weight: bold; 
+ font-size: 20px; 
+ } 
+ .manu 
+ { 
+ height: 25px; 
+ width: 100%; 
+ text-align: center; 
+ background-color: black; 
+opacity: 0.7; 
+}
+ul li 
+ { 
+ 
+ display: inline; 
+ } 
+ ul li a 
+ { 
+ margin-top: 15px; 
+ margin-left: 50px; 
+ color: white; 
+ font-size: 15px; 
+ font-weight: bold; 
+ text-decoration:none; 
+ } 
+ ul li a:hover 
+ { 
+ color: blue; 
+ } 
+ main 
+ { 
+ margin-top: 25px; 
+ width: 100%; 
+ height: 300px;
+border:1px solid white; 
+ background-image: url('tourplace.png'); 
+ background-size: cover; 
+ font-size: 20px; 
+ } 
+ .content 
+ { 
+ margin-top: 100px; 
+ text-align: center; 
+ text-shadow: 1px 1px 2px 1px orange; 
+ } 
+ .service 
+ { 
+ width: 100%; 
+ height: 200px; 
+ 
+ } 
+ .color 
+ { 
+ } 
+ .img1 
+ { 
+ width: 30%; 
+ height: 150px; float: left; 
+ margin-left: 2.5%; 
+ margin-top: 10px; 
+ background-color: white; 
+ box-shadow: 1px 1px 2px 1px black; 
+ text-align: center; 
+ } 
+ .img1:hover 
+ { 
+ background-color: green; 
+} 
+ .img1 h2 
+ { 
+ } 
+ .regi 
+ { 
+ width: 100%; 
+ height: 100px; 
+ } 
+ .now 
+ { 
+ width: 90%; 
+ height: 80px; 
+background-color: grey; 
+ margin:auto; 
+ margin-top: 10px; 
+ box-shadow: 1px 1px 2px 1px green; 
+ text-align: center; 
+ border-radius: 10px; 
+ } 
+ .now h3 
+ { 
+ padding-top: 15px; 
+ } 
+ .now input[type="submit"] 
+ { 
+ margin-top: 10px; 
+ font-size: 15px; 
+ font-weight: bold; 
+ height: 20px; 
+ width: 150px; 
+ border: none; 
+ border-radius: 15px; 
+ background-color: lightblue; 
+ } 
+ .now input[type="submit"]:hover 
+{
+color: blue; 
+background-color: white; 
+ } 
+ section{ 
+ width: 100%; 
+ height: 600px; 
+ } 
+ .write 
+ { 
+ width: 100%; 
+ height: 30px; 
+ } 
+ .write h3 
+ { 
+ margin-left: 20px; 
+ font-weight: bold; 
+ font-size: 30px; 
+ } 
+ .img{ 
+ width: 300px; 
+ float: left; 
+ border:100px; 
+ height: 200px; 
+ border: 2px solid white; 
+ margin-left: 30px; 
+ margin-top: 30px; 
+font-size: 20px; 
+ text-align: center; 
+ font-weight: bold; 
+ 
+ } 
+ .img img 
+ { 
+ width:300px; 
+ height: 200px; 
+ opacity: 0.5; 
+ } 
+ .img img:hover 
+ { 
+ opacity: 10; 
+ } 
+ footer 
+ { 
+ width: 100%; 
+ height: 250px; 
+ background-color: grey; 
+ text-align: center; 
+ } 
+ footer h2 
+ { 
+ padding-top: 15px; 
+} 
+ footer input[type="text"] 
+ { 
+ display: block; 
+ margin:auto; 
+ width: 300px; 
+ height: 20px; 
+ margin-top: 5px; 
+ border-radius: 5px; 
+ border: none; 
+ padding: 5px; 
+ } 
+ footer input[type="submit"] 
+ { 
+ width: 200px; 
+ height: 25px; 
+ margin-top: 15px; 
+ border: none; 
+ border-radius: 15px; 
+ background-color: black; 
+ color: white; 
+ font-size: 20px; 
+ text-transform: capitalize; 
+}
+footer input[type="submit"]:hover 
+ { 
+ background-color: green; 
+ } 
+ </style> 
+ 
+</head> 
+<body> 
+ <header> 
+ <div class="first"> 
+ <div class="logo"><h2>Destiny</h2></div> 
+ <div class="social">Insta FB Twitter</div> 
+ </div> 
+ <div class="manu"> 
+ <ul> 
+ <li><a href="#">Home</a></li> 
+ <li><a href="#">Abour Us</a></li> 
+ <li><a href="#">Destinations</a></li> 
+ <li><a href="#">Places to stay</a></li> 
+ <li><a href="#">Contact</a></li> 
+</ul> 
+ </div> 
+ </header> 
+ <main class="back"> 
+<div class="content"> 
+ <h4>Tour & Travels</h4> 
+ <h2>Amazing Places on earth</h2> 
+ </div> 
+ </main> 
+ <div class="service"> 
+ <div class="color"> 
+ <div class="img1"> 
+ <img src=""> 
+ <h2>Support</h2> 
+ <p>welcome welcome welcome welcome welcome welcome welcome welcome welcome 
+welcome welcome welcome welcome</p> 
+ </div> 
+ <div class="img1"> 
+ <img src=""> 
+ <h2>Destinations</h2> 
+ <p>welcome welcome welcome welcome welcome welcome welcome welcome welcome 
+welcome welcome welcome welcome</p> 
+ </div> 
+ <div class="img1"> 
+ <img src=""> 
+ <h2>Holiday</h2> 
+ <p>welcome welcome welcome welcome welcome welcome welcome welcome welcome 
+welcome welcome welcome welcome</p> 
+ </div> 
+ </div> 
+ </div> 
+<div class="regi"> 
+ <div class="now"> 
+ <h3>THE TRAVAL SCOUT BEST TRAVAL WEBSITE IN GUJARAT</h3> 
+ <input type="submit" name="reg" value="REGISTER NOW"> 
+ </div> 
+ </div> 
+ <section> 
+ <div class="write"> 
+ <h3>Populer Places-------------------</h3> 
+ </div> 
+ <div class="photos"> 
+ <div class="img"> 
+ <img src="rankivav.png"> 
+ <p>Rankivav-Patan</p> 
+ </div> 
+ <div class="img"> 
+ <img src="vvp.png"> 
+ <p>Vijay Vilas Place-Kutch</p> 
+ </div> 
+ <div class="img"> 
+ <img src="sontp.png"> 
+ <p>Sun Temple-Mahesana</p> 
+ </div> 
+ <div class="img"> 
+ <img src="msu.png"> 
+<p>MSU Vadodara</p> 
+ </div> 
+ <div class="img"> 
+ <img src="ranimhel.png"> 
+ <p>Rani Mahel</p> 
+ </div> 
+ <div class="img"> 
+ <img src="vgeat.png"> 
+ <p>Kirti Toran-Vadnagar</p> 
+ </div> 
+ </div> 
+ </section> 
+ <footer> 
+ <h2>Contact Us</h2> 
+ <input type="text" name="name" placeholder="Enter your name"> 
+ <input type="text" name="email" placeholder="Enter Email Id"> 
+ <input type="text" name="address" placeholder="Enter address"> 
+ <input type="text" name="number" placeholder="Enter Phone number"> 
+<input type="submit" name="sub" value="click here"> 
+ </footer> 
+</body> 
+</html
